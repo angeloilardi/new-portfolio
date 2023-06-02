40a25002a3931ef326961498f16fedb396748aa6
Angelo Ilardi <ange448@gmail.com>
2023-06-02 08:42:01 +0100

    adjusted project cards

--- app/components/ProjectCard.tsx
+++ app/components/ProjectCard.tsx
@@ -1,39 +1,45 @@
 export default function ProjectCard({img, url, name, description}) {
     return (
-      <div className="max-w-sm min-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700 mb-6 mx-auto">
-        <a href={url}>
-          <img className="rounded-t-lg min-h-60 object-cover" src={img} alt="" />
+      <div className="max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700 mb-6 mx-auto md:flex md:h-max md:flex-row-reverse md:max-w-3xl md:min-h-fit md:w-8/12">
+        <a href={url} className="basis-1/2 shrink-1 max-w-sm">
+          <img
+            className="rounded-t-lg min-h-60 object-cover"
+            src={img}
+            alt=""
+          />
         </a>
-        <div className="p-5 flex flex-col">
+        <div className="p-5 flex flex-col basis-1/2 shrink-1 max-w-sm">
           <a href={url}>
             <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
-              {name}
+              {name.toUpperCase()}
             </h5>
           </a>
           <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">
             {description}
           </p>
-                <div className="my-auto
-          ">
-              <a
-                href={url}
-                className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+          <div
+            className="my-auto
+          "
+          >
+            <a
+              href={url}
+              className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+            >
+              Read more
+              <svg
+                aria-hidden="true"
+                className="w-4 h-4 ml-2 -mr-1"
+                fill="currentColor"
+                viewBox="0 0 20 20"
+                xmlns="http://www.w3.org/2000/svg"
               >
-                Read more
-                <svg
-                  aria-hidden="true"
-                  className="w-4 h-4 ml-2 -mr-1"
-                  fill="currentColor"
-                  viewBox="0 0 20 20"
-                  xmlns="http://www.w3.org/2000/svg"
-                >
-                  <path
-                    fill-rule="evenodd"
-                    d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z"
-                    clip-rule="evenodd"
-                  ></path>
-                </svg>
-              </a>
+                <path
+                  fill-rule="evenodd"
+                  d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z"
+                  clip-rule="evenodd"
+                ></path>
+              </svg>
+            </a>
           </div>
         </div>
       </div>
--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -7,7 +7,7 @@ export default function Projects() {
       <>
         <Navbar></Navbar>
         <div className="bg-blue-200 min-h-screen py-12">
-          <div className="sm:flex sm:justify-center sm:flex-wrap sm:align-middle">
+          <div className="sm:flex sm:justify-center sm:flex-wrap sm:flex-col sm:align-middle">
             {projects.map((project) => {
               return (
                 <ProjectCard
