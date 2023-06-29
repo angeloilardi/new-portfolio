bf9edd2e21b5351e88fc163390714b2294b036e4
Angelo Ilardi <ange448@gmail.com>
2023-06-29 19:12:20 +0100

    added send buton to form

--- app/components/ContactForm.tsx
+++ app/components/ContactForm.tsx
@@ -13,53 +13,62 @@ export default function Form() {
         </p>
         <div className="mt-8 max-w-lg">
           <div className="grid grid-cols-1 gap-6">
-            <label className="block">
-              <span className="text-gray-700">Full name</span>
-              <input
-                type="text"
-                className="
-                    mt-1
-                    block
-                    w-full
-                    rounded-md
-                    border-gray-300
-                    shadow-sm
-                    focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50
-                  "
-                placeholder=""
-              />
-            </label>
-            <label className="block">
-              <span className="text-gray-700">Email address</span>
-              <input
-                type="email"
-                className="
-                    mt-1
-                    block
-                    w-full
-                    rounded-md
-                    border-gray-300
-                    shadow-sm
-                    focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50
-                  "
-                placeholder=""
-              />
-            </label>
-            <label className="block">
-              <span className="text-gray-700">Your message</span>
-              <textarea
-                className="
-                    mt-1
-                    block
-                    w-full
-                    rounded-md
-                    border-gray-300
-                    shadow-sm
-                    focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50
-                  "
-                rows={3}
-              ></textarea>
-            </label>
+            <form
+              action="https://formsubmit.co/angeloilardi.dev@gmail.com"
+              method="POST"
+            >
+              <label className="block">
+                <span className="text-gray-700">Full name</span>
+                <input
+                  name="name"
+                  type="text"
+                  className="
+                      mt-1
+                      block
+                      w-full
+                      rounded-md
+                      border-gray-300
+                      shadow-sm
+                      focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50
+                    "
+                  placeholder=""
+                />
+              </label>
+              <label className="block">
+                <span className="text-gray-700">Email address</span>
+                <input
+                  name="email"
+                  type="email"
+                  className="
+                      mt-1
+                      block
+                      w-full
+                      rounded-md
+                      border-gray-300
+                      shadow-sm
+                      focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50
+                    "
+                  placeholder=""
+                />
+              </label>
+              <label className="block">
+                <span className="text-gray-700">Your message</span>
+                <textarea
+                  name="message"
+                  className="
+                      mt-1
+                      block
+                      w-full
+                      rounded-md
+                      border-gray-300
+                      shadow-sm
+                      focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50
+                    "
+                  rows={3}
+                ></textarea>
+              </label>
+              <button className="rounded-full bg-black text-white p-2 mt-3" type="submit">Send</button>
+            </form>
             <div className="block"></div>
           </div>
           <p>
@@ -68,7 +77,10 @@ export default function Form() {
               <i className="fa-regular fa-envelope fa-lg"></i>
             </a>{" "}
             or message me via{" "}
-            <a href="https://www.linkedin.com/in/angelo-ilardi/" target="_blank">
+            <a
+              href="https://www.linkedin.com/in/angelo-ilardi/"
+              target="_blank"
+            >
               <i className="fa-brands fa-linkedin fa-lg"></i>
             </a>
           </p>
