b79988046602e14883a64b17bdfc3ec20de8d1be
Angelo Ilardi <ange448@gmail.com>
2024-03-04 22:14:22 +0000

    form validation

--- app/components/ContactForm.tsx
+++ app/components/ContactForm.tsx
@@ -13,7 +13,7 @@ export default function Form() {
           <form
             action="https://formsubmit.co/2b5a0c554cbe020fae0c799111bac22a"
             method="POST"
-            className="text-black dark:text-pink-200"
+            className="text-black dark:text-pink-200 group"
           >
             <div className="grid grid-cols-1 gap-6">
               <label className="block">
@@ -30,14 +30,19 @@ export default function Form() {
                       shadow-sm
                       focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50
                     dark:bg-slate-600 dark:border-pink-200"
-                  placeholder=""
+                  placeholder=" "
+                  required
                 />
+                <p className="peer-[&:not(:placeholder-shown):not(:focus):invalid]:block hidden italic text-xs mt-1">
+                  Please enter your name
+                </p>
               </label>
               <label className="block">
                 <span className="">Email address</span>
                 <input
                   name="email"
                   type="email"
+                  pattern="[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$"
                   className="
                       mt-1
                       block
@@ -46,9 +51,13 @@ export default function Form() {
                       border-gray-300
                       shadow-sm
                       focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50
-                    dark:bg-slate-600 dark:border-pink-200"
-                  placeholder=""
+                    dark:bg-slate-600 dark:border-pink-200 peer"
+                  placeholder=" "
+                  required
                 />
+                <p className="peer-[&:not(:placeholder-shown):not(:focus):invalid]:block hidden italic text-xs mt-1">
+                  Please enter a valid email address
+                </p>
               </label>
               <label className="block">
                 <span className="">Your message</span>
@@ -64,6 +73,7 @@ export default function Form() {
                       focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50
                     dark:bg-slate-600 dark:border-pink-200"
                   rows={3}
+                  required
                 ></textarea>
               </label>
             </div>
